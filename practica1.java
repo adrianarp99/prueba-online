66e842b36f3897bcc798d9aae03c29754fe0b792
Adrian Ariza <adrianarizaponce@gmail.com>
2023-11-14 16:12:36 +0100

    version definitiva

--- practica1.java
+++ practica1.java
@@ -4,12 +4,7 @@ System.out.println("*********")
 System.out.println("hola mundo")
 System.out.println("*********");
 
-int num1=3;
-int num2=2;
-int resultado=0;
 
-resultado=num1+num2;
-system.out.println("el resultado es " +resultado);
 
 
 }
