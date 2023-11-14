dfdbed6fbf0fe1bdfdef2e2be18b728d04983a74
Adrian Ariza <adrianarizaponce@gmail.com>
2023-11-14 16:10:16 +0100

    operacion

--- practica1.java
+++ practica1.java
@@ -1,7 +1,15 @@
 public static void main(String[] args) {
     
+System.out.println("*********")
+System.out.println("hola mundo")
 System.out.println("*********");
-System.out.println("hola mundo");
-System.out.println("*********");
+
+int num1=3;
+int num2=2;
+int resultado=0;
+
+resultado=num1+num2;
+system.out.println("el resultado es " +resultado);
+
 
 }
